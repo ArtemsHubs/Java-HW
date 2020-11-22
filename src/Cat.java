0e0fa8f206b94cc3a03a4ce3bfc093fb282b53a7
ArtemsHubs <Dolgikhart@mail.ru>
2020-11-22 15:34:24 +0300

    homeworkLesson7.Done

--- src/Cat.java
+++ src/Cat.java
@@ -1,11 +1,27 @@
 public class Cat {
 private String name;
     private int appetite;
+    private boolean fedUp;
     public Cat(String name,int appetite){
         this.name= name;
         this.appetite= appetite;
+        this.fedUp= fedUp;
     }
     public void eat(Plate p){
         p.decreaseFood(appetite);
     }
+    public void IsFedUp(boolean full){
+        fedUp=full;
+        System.out.println(fedUp);
+    }
+    public void IsItEnough(Plate p){
+        fedUp=Plate.en;
+        System.out.println(fedUp);
+    }
+    public String getName(){
+        return name;
+    }
 }
+
+
+
